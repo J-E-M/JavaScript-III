aa22d27b0fc5e26116c6df9cc2b21ffbe17d6d1f
J-E-M <javonmccoy22@gmail.com>
2019-03-06 17:07:59 -0600

    this.js is done, missing prototype methods in prototype.js, everything else in done.

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1.window binding: when the 'this' keyword is used in the global scope, it's default value will be the window object.
+* 2.implicit binding:when calling a function with a dot, the object before the dot is what 'this' will be bound to.
+* 3. explicit binding: explicitly telling a function what object to use for this, by using .call or .apply
+* 4.
 *
 * write out a code example of each explanation above
 */
@@ -13,9 +13,26 @@
 
 // code example for Window Binding
 
+var myFunction = function() {
+   console.log(this);
+}
+
+myFunction();
+
 // Principle 2
 
 // code example for Implicit Binding
+
+function imp(){
+	console.log(this.a);
+}
+
+var obj = {
+	a:2,
+	imp:imp
+};
+
+obj.imp()
 
 // Principle 3
 
@@ -24,3 +41,13 @@
 // Principle 4
 
 // code example for Explicit Binding
+
+function sayName() {
+	console.log(this.name);
+}
+
+var person = {
+	name:'Javon'
+}
+
+sayName.call(person);
